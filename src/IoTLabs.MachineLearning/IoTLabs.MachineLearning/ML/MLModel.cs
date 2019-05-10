14aee6fc5d2c2964d62dbe5d8ebac415cbdcea71
Jourdan Templeton (AWARE) <jourdan.templeton@awaregroup.com>
2019-05-10 15:18:16 -0700

    Added machine learning project

---
+++ src/IoTLabs.MachineLearning/IoTLabs.MachineLearning/ML/MLModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.AI.MachineLearning;
+using Windows.Storage.Streams;
+
+namespace IoTLabs.MachineLearning.ML
+{
+    public sealed class MLModel
+    {
+        private LearningModel _model;
+        private LearningModelSession _session;
+        private LearningModelBinding _binding;
+
+        public async Task<MLModelOutput> EvaluateAsync(MLModelInput input)
+        {
+            _binding.Bind("float_input", input.FloatInput);
+
+            var id = Guid.NewGuid().ToString();
+            var result = await _session.EvaluateAsync(_binding, id);
+
+            return new MLModelOutput
+            {
+                Variable = result.Outputs["variable"] as TensorFloat
+            };
+        }
+
+        public static async Task<MLModel> CreateFromStreamAsync(IRandomAccessStreamReference stream)
+        {
+            var device = new LearningModelDevice(LearningModelDeviceKind.Cpu);
+
+            var model = new MLModel();
+            model._model = await LearningModel.LoadFromStreamAsync(stream);
+            model._session = new LearningModelSession(model._model, device);
+            model._binding = new LearningModelBinding(model._session);
+
+            return model;
+        }
+    }
+}
---
+++ src/IoTLabs.MachineLearning/IoTLabs.MachineLearning/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using IoTLabs.MachineLearning.ML;
+using System;
+using System.Linq;
+using Windows.AI.MachineLearning;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+namespace IoTLabs.MachineLearning
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //load and initialize model
+                var modelFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri($"ms-appx:///Assets/model.onnx"));
+                var model = await MLModel.CreateFromStreamAsync(modelFile as IRandomAccessStreamReference);
+
+                //make prediction
+                var modelOutput = await model.EvaluateAsync(new MLModelInput()
+                {
+                    FloatInput = TensorFloat.CreateFromArray(new long[2] { 1, 4 }, new float[4] { float.Parse(TextBox1.Text), float.Parse(TextBox2.Text), float.Parse(TextBox3.Text), float.Parse(TextBox4.Text) })
+                });
+
+                //display results
+                var noWifiDialog = new ContentDialog
+                {
+                    Title = "Machine Learning Module Result",
+                    Content = $"{string.Join(",", modelOutput.Variable.GetAsVectorView().Select(f => f.ToString()))}",
+                    CloseButtonText = "Close"
+                };
+
+                await noWifiDialog.ShowAsync();
+            }
+            catch (Exception)
+            {
+                ContentDialog noWifiDialog = new ContentDialog
+                {
+                    Title = "Something isn't working",
+                    Content = "Did you enter numbers?",
+                    CloseButtonText = "Try again"
+                };
+
+                try
+                {
+                    await noWifiDialog.ShowAsync();
+                }
+                catch { }
+            }
+        }
+    }
+}

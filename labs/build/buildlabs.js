c3886099c393f59402d99e9a208b7fe48a5e6ca1
Nathan Holland (AWARE) <nathan.holland@awaregroup.com>
2019-05-13 09:12:48 -0500

    Fix concurrency issue

--- labs/build/buildlabs.js
+++ labs/build/buildlabs.js
@@ -4,19 +4,36 @@ const path = require('path');
 let options = {
 	//styles: path.join(__dirname, '/node_modules/github-markdown-css/github-markdown.css'),
 	ghStyle : true,
-	defaultStyle : true,
 	pdf: {
 		format: 'A4',
-		orientation: 'portrait'
+		orientation: 'portrait',
+		border: {
+				top:60,
+				right:60,
+				bottom:60,
+				left:60
+		}
+		}
 	}
-};
+
 
 const labsFolder = __dirname+'/..';
 const fs = require('fs');
 
+
+
 fs.readdir(labsFolder, (err, files) => {
-	files.forEach(file => {
-		if (file.endsWith(".md")) {
+	createPdf(files);
+});
+
+function createPdf(files){
+	if(files[0]){
+		let file = files[0]
+		if (!file.endsWith(".md")) {
+			createPdf(files.slice(1, files.length))
+		}
+		else{
+			console.log(file);
 			sourcePath = path.join(labsFolder, file);
 			console.log(sourcePath);
 			fileOptions = {
@@ -26,10 +43,10 @@ fs.readdir(labsFolder, (err, files) => {
 			}
 			mdpdf.convert(fileOptions).then((pdfPath) => {
 				console.log('PDF Path:', pdfPath);
+				createPdf(files.slice(1, files.length))
 			}).catch((err) => {
 				console.error(err);
 			});
 		}
-	});
-
-});
+	};
+}

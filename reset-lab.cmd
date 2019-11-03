97695aa2cfddef2ec90d5154fc3f31bb729e6322
Nathan Holland <6608414+NathanNZ@users.noreply.github.com>
2019-11-02 23:31:14 -0700

    Update reset-lab.cmd

--- reset-lab.cmd
+++ reset-lab.cmd
@@ -1,8 +1,9 @@
 @ECHO OFF
 SET base=%~dp0
 echo Base directory: %base:~0,-1%
-SET DRIVERSDIR=%base%
-CD "%DRIVERSDIR%"
+SET CURDIR=%base%
+CD /D "%CURDIR%"
 
 git clean -xdf
 git reset --hard
+git pull
